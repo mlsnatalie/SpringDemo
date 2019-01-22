d66aaff71614bc51185cdc73985c6459c6c139aa
mlsnatalie <mlsnatalie@163.com>
2019-01-22 18:20:57 +0800

    init

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @RequestMapping("/say")
+    public String sayHello() {
+        return "hello world";
+    }
+}
